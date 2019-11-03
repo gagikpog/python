42a772150d1d568f4141331fa2a3d023ef038e46
Gagik <gagikpog@gmail.com>
2019-11-03 22:27:02 +0500

    Revert auth
    This reverts commit 50ddf44d5a43df2910a7e5a377e1330712aa0084.

--- app/__init__.py
+++ app/__init__.py
@@ -1,29 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate, MigrateCommand
+from flask_migrate import Migrate
 from config import Config
-from flask_script import Manager
-from flask_admin import Admin
-#from flask_admin.contrib.sqla import ModelView
-from flask_security import SQLAlchemyUserDatastore #Хранилище данных SQLAlchemy
-from flask_security import Security
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-manager=Manager(app)
-manager.add_command('db',MigrateCommand)
-#login.login_view = 'login'
-from app import routes, models
-#from app.routes import user_route, routes
 
-#администрирование- продумать позже
-### ADMIN ###
-#from models import *
-#admin = Admin(app)
-#admin.add_view(ModelView())
-
-### Flask-security ###
-#user_datastore = SQLAlchemyUserDatastore(db, User, Role)
-#security = Security(app, user_datastore)
+from app import models
+from app.routes import user_route, routes
--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,8 +1,7 @@
 from flask import render_template, flash, redirect, url_for, send_from_directory, request, jsonify, abort
-from app import app,db
-from app.forms import LoginForm, RegistrationForm
+from app import app, db
+from app.forms import LoginForm
 from app.models import User, Bill
-
 
 
 @app.route('/')
@@ -19,31 +18,11 @@ def login():
         return redirect(url_for('index'))
     return render_template('login.html',  title='Sign In', form=form)
 
-@app.route('/logout')
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data)
-        user.set_password(form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        flash('Registration Ok!')
-        return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form) 
 
 @app.route('/src/<path:path>')
 def src(path):
     return send_from_directory('src', path)
 
-
 @app.errorhandler(404)
 def not_found(error):
-   return render_template('notFind.html')
-
+    return render_template('notFind.html')
